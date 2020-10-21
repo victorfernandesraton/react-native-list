42b53fbc94a18b2fd95d74372ef935733ecdccc7
victorfernandesraton <vfbraton@gmail.com>
2020-10-20 21:47:36 -0300

    first flatList

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+const renderItem = ({ item }) => (
+  <View style={{height: 200}}>
+    <Text>{`${item.name}`}</Text>
+  </View>
+);
 export default function App() {
+  const [data, setData] = useState([
+    {
+      id: "1",
+      name: "victor",
+    },
+    {
+      id: "2",
+      name: "victor",
+    },
+    {
+      id: "3",
+      name: "victor",
+    },
+    {
+      id: "010",
+      name: "victor",
+    },
+    {
+      id: "4",
+      name: "victor",
+    },
+    {
+      id: "5",
+      name: "victor",
+    },
+    {
+      id: "6",
+      name: "victor",
+    },
+  ]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste meu caro</Text>
+      <Text>{data.length}</Text>
+      <FlatList
+        data={data}
+        extraData={data}
+        maxToRenderPerBatch={3}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        onEndReached={() => {
+          setData([{id: data.length + 1, name: "teste"}, ...data])}
+        }
+        onEndReachedThreshold={1}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +61,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
