5ade00cb856dd088cc0ab6ade55579842971423c
victorfernandesraton <vfbraton@gmail.com>
2020-10-27 23:35:34 -0300

    simple navbar

--- App.js
+++ App.js
@@ -1,68 +1,16 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
-const renderItem = ({ item }) => (
-  <View style={{height: 200}}>
-    <Text>{`${item.name}`}</Text>
-  </View>
-);
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Index from './src/pages'
+const Stack = createStackNavigator();
 export default function App() {
-  const [data, setData] = useState([
-    {
-      id: "1",
-      name: "victor",
-    },
-    {
-      id: "2",
-      name: "victor",
-    },
-    {
-      id: "3",
-      name: "victor",
-    },
-    {
-      id: "010",
-      name: "victor",
-    },
-    {
-      id: "4",
-      name: "victor",
-    },
-    {
-      id: "5",
-      name: "victor",
-    },
-    {
-      id: "6",
-      name: "victor",
-    },
-  ]);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Teste meu caro</Text>
-      <Text>{data.length}</Text>
-      <FlatList
-        data={data}
-        extraData={data}
-        maxToRenderPerBatch={3}
-        keyExtractor={(item) => item.id}
-        renderItem={renderItem}
-        onEndReached={() => {
-          setData([{id: data.length + 1, name: "teste"}, ...data])}
-        }
-        onEndReachedThreshold={1}
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Index}></Stack.Screen>
+      </Stack.Navigator>
+      <StatusBar style='auto' />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ src/pages/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+const renderItem = ({ item }) => (
+  <View style={{height: 200}}>
+    <Text>{`${item.name}`}</Text>
+  </View>
+);
+export default function Index() {
+  const [data, setData] = useState([
+    {
+      id: "1",
+      name: "victor",
+    },
+    {
+      id: "2",
+      name: "victor",
+    },
+    {
+      id: "3",
+      name: "victor",
+    },
+    {
+      id: "010",
+      name: "victor",
+    },
+    {
+      id: "4",
+      name: "victor",
+    },
+    {
+      id: "5",
+      name: "victor",
+    },
+    {
+      id: "6",
+      name: "victor",
+    },
+  ]);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste meu caro</Text>
+      <Text>{data.length}</Text>
+      <FlatList
+        data={data}
+        extraData={data}
+        // maxToRenderPerBatch={3}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        onEndReached={() => {
+          setData([{id: data.length + 1, name: "teste"}, ...data])}
+        }
+        onEndReachedThreshold={1}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
