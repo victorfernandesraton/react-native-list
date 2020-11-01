af94fa4c306d7711538c1f78d254344dff3b8cd2
victorfernandesraton <vfbraton@gmail.com>
2020-11-01 18:49:33 -0300

    basic drawer

---
+++ src/components/drawer/Drawer-constants.js
@@ -0,0 +1,10 @@
+import SpotfyScreen from '../../screen/spotfy-screen';
+
+export const DrawerItens = [
+  {
+    name: 'sporty-ascreem',
+    title: "Spotfy",
+    component: SpotfyScreen,
+    default: true,
+  },
+];
--- src/components/drawer/DrawerView-container.js
+++ src/components/drawer/DrawerView-container.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import SpotfyScreen from '../../screen/spotfy-screen';
 const Drawer = createDrawerNavigator();
+
+import { DrawerItens } from './Drawer-constants';
 function DrawerView() {
   return (
-    <Drawer.Navigator iniinitialRouteNametial="spotfy-app">
-      <Drawer.Screen name="spotfy-app" component={SpotfyScreen} />
+    <Drawer.Navigator iniinitialRouteNametial={DrawerItens.find(el => el.default === true).name}>
+      {DrawerItens.map((item, key) => {
+        return (
+          <Drawer.Screen
+            name={item.name}
+            component={item.component}
+            options={{
+              title: item.title,
+              drawerLabel: item.title
+            }}
+            key={key}
+          />
+        );
+      })}
     </Drawer.Navigator>
   );
 }
