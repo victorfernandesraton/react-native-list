d5ec91a058531d30fb94f12b30f3d30dacc25b31
victorfernandesraton <vfbraton@gmail.com>
2020-11-03 23:41:29 -0300

    basic topbar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import {StatusBar} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 
 import Drawer from './src/components/drawer/DrawerView-container';
 export default function App() {
   return (
     <NavigationContainer>
+			<StatusBar />
       <Drawer />
     </NavigationContainer>
   );
--- src/components/topbar/TopBarView-container.js
+++ src/components/topbar/TopBarView-container.js
@@ -19,7 +19,10 @@ function TopBarView(props) {
 const styles = StyleSheet.create({
 	container: {
 		alignSelf: 'stretch',
-		height: 52,
+		height: 64,
+		shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2000 },
+    shadowOpacity: 0,
 		flexDirection: 'row', // row
 		alignItems: 'center',
 		justifyContent: 'center', // center, space-around
