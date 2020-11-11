d2d712b264dbe624b87b4b81293bcf8d3c41d907
victorfernandesraton <vfbraton@gmail.com>
2020-11-10 22:00:16 -0300

    pass item for screen

--- src/components/turism/turismMain.js
+++ src/components/turism/turismMain.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import api from '../turism/api';
 import { withNavigation } from 'react-navigation';
 
-
 import {
 	View,
 	Text,
@@ -12,35 +11,34 @@ import {
 	Image,
 } from 'react-native';
 
- class turismMain extends Component {
+class turismMain extends Component {
 	state = {
 		docs: [],
 		limit: 3,
 		page: 1,
 	};
 
-	
 	componentDidMount() {
 		this.loadProducts();
 	}
 
 	loadProducts = async () => {
-        const response = await api.get('/lugares');
-        if (this.state.docs.length < 20) {
-					this.setState({
-                docs: [
-									...this.state.docs,
-									...response.data.slice(
-										this.state.limit * (this.state.page - 1),
-                        this.state.limit * this.state.page
-												),
-                ],
-                page: this.state.page + 1,
-            });
-					}
-    };
+		const response = await api.get('/lugares');
+		if (this.state.docs.length < 20) {
+			this.setState({
+				docs: [
+					...this.state.docs,
+					...response.data.slice(
+						this.state.limit * (this.state.page - 1),
+						this.state.limit * this.state.page
+					),
+				],
+				page: this.state.page + 1,
+			});
+		}
+	};
 
-		render() {
+	render() {
 		return (
 			<View style={styles.container}>
 				<FlatList
@@ -48,7 +46,9 @@ import {
 					data={this.state.docs}
 					keyExtractor={(item) => item.id}
 					onEndReached={this.loadProducts}
-					renderItem={(item) => <RenderItem item={item} navigation={ this.props.navigation } />}
+					renderItem={(item) => (
+						<RenderItem item={item} navigation={this.props.navigation} />
+					)}
 				/>
 			</View>
 		);
@@ -56,17 +56,34 @@ import {
 }
 
 const RenderItem = ({ item, navigation }) => {
-	// console.log(navigation, item)
-
+	console.log(item.item);
 	return (
 		<View style={styles.productContainer}>
 			<Text style={styles.productTitle}>{item.item.name}</Text>
 			<Text style={styles.productDescription}>{item.item.descrition}</Text>
-			<View style={{ display: 'flex', justifyContent:'center', alignItems: 'center' }}>
-				<Image source={{ uri: item.item.image }} width={200} height={200} resizeMode='cover'/>
+			<View
+				style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<Image
+					source={{ uri: item.item.image }}
+					width={200}
+					height={200}
+					resizeMode="cover"
+				/>
 			</View>
 
-			<TouchableOpacity onPress={() => { navigation.navigate('turism-info') }} style={styles.productButton}>
+			<TouchableOpacity
+				onPress={() => {
+					navigation.navigate('turism-info',{
+						item: item.item
+					});
+				}}
+				style={styles.productButton}
+			>
 				<Text>More</Text>
 			</TouchableOpacity>
 		</View>
@@ -115,6 +132,5 @@ const styles = StyleSheet.create({
 		marginTop: 10,
 	},
 });
-
 
 export default withNavigation(turismMain);
