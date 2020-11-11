e896bdfd5885910f6096bcc082937ec42c0bca5b
Maurilio David S. Conceicao <maurilio_david@hotmail.com>
2020-11-10 21:30:45 -0300

    lista de contato

---
+++ src/components/Contatos/contactMain.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet, Image,} from 'react-native';
+export default class telaContato extends Component {
+    
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+    loadUsers = () => {
+
+        fetch("https://randomuser.me/api/?results=20")
+            .then( res => res.json() )
+            .then( res => {
+                this.setState ({
+                    data: res.results || []
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.loadUsers();
+    }
+
+    render(){
+        return(
+            <View style={StyleSheet.container}> 
+                
+                <FlatList
+                    data={this.state.data}
+                    renderItem={({item}) => (
+                        
+                        <View style={styles.line}>
+                            <Image
+                                source={{ uri: item.picture.thumbnail }}
+                                style={styles.avatar}
+                            />
+
+                            <View style={styles.info}>
+                                <Text style={styles.email}>{item.email}</Text>
+                                <Text style={styles.name}>{item.name.first} {item.name.last}</Text>
+                            </View>
+
+                        </View>
+                    )}
+                    keyExtractor={ item => item.email}
+                    />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    marginLeft: 10,
+    backgroundColor: "#FFF",
+    borderTopWidth:0,
+    borderBottomWidth: 0
+  },
+  logo: {
+    width: 100,
+    height:100,
+    borderRadius: 100
+  },
+  line: {
+    height: 50,
+    flexDirection: "row",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    marginRigth: 10,
+    alignSelf: "center"
+  },
+  info: {
+      flexDirection: "column",
+      justifyContent: "flex-start"
+  }, 
+  name: {
+      fontSize: 12
+  },
+  email: {
+    fontSize: 14,
+    fontWeight: "bold"
+  }
+});
--- src/components/drawer/Drawer-constants.js
+++ src/components/drawer/Drawer-constants.js
@@ -1,6 +1,7 @@
 import TurismScreen from '../../screen/turism-screen';
 //import GiphyScreen from '../../screen/giphy-screen';
 import GiphyRoutes from '../../components/giphy/Giphy-routes';
+import TelaContato from '../../screen/telaContatos';
 
 export const DrawerItens = [
   {
@@ -13,5 +14,10 @@ export const DrawerItens = [
 		name: 'giphy-routes',
 		title: 'GIPHY',
 		component: GiphyRoutes,
+	},
+	{
+		name: 'telaContatos',
+		title: 'My Contats',
+		component: TelaContato,
 	}
 ];
